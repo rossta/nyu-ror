e629f9f3ccfc5cbb1e5fcfde9524108a7d0743c6
Ross Kaffenberger <rosskaff@gmail.com>
2011-02-08 10:58:51 -0500

    Add conventions for executable

--- PS0/guess_num.rb
+++ PS0/guess_num.rb
@@ -1,15 +1,20 @@
+#!/usr/local/bin/ruby -w
+
 $:.unshift File.join(File.dirname(__FILE__), "lib")
 require 'guess_num'
 
-max = (ARGV.shift || 100).to_i
-guess_num = GuessNum.new(max)
+if $0 == __FILE__
+  max = (ARGV.shift || 100).to_i
+  guess_num = GuessNum.new(max)
 
-puts guess_num.last_message
-
-while guess = gets.chomp
-  break if guess_num.correct?(guess)
   puts guess_num.last_message
+
+  while guess = gets.chomp
+    break if guess_num.correct?(guess)
+    puts guess_num.last_message
+  end
+
+  puts guess_num.last_message
+  puts '==fin=='
 end
 
-puts guess_num.last_message
-puts '==fin=='
